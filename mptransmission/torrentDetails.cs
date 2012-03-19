69c0bdf4cd1e69fe9509610565980f591b62039e
lp.richards@gmail.com <lp.richards@gmail.com@5985aef6-3f37-4e81-8bd8-96803256ff5d>
2012-03-19 23:01:49 +0000

    Skin Improvements Added Torrent Details Page on listItem click Added basic context menu Code improvements (variables.cs) Timer improvements

---
+++ mptransmission/torrentDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaPortal.GUI.Library;
+using MediaPortal.Dialogs;
+using MediaPortal.Configuration;
+using MediaPortal.Profile;
+using mptransmission.Settings;
+using System.Timers;
+
+namespace mptransmission
+{
+    public class torrentDetails : GUIWindow
+    {
+
+        public override int GetID
+        {
+            get
+            {
+                return 56348;
+            }
+
+            set
+            {
+            }
+        }
+
+        public override bool Init()
+        {
+            return Load(GUIGraphicsContext.Skin + @"\mptransmission.Details.xml");
+        }
+
+        protected override void OnPageLoad()
+        {
+            mptransmission.aTimer.Start();
+        }
+
+        protected override void OnPageDestroy(int new_windowId)
+        {
+            base.OnPageDestroy(new_windowId);
+            mptransmission.aTimer.Stop();
+        }
+    }
+}
---
+++ mptransmission/variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaPortal.GUI.Library;
+
+namespace mptransmission
+{
+   public static class variables
+    {
+        public static int selTorrent = 0;
+        //public static int listSize = 0;
+        public static int activeTorrents = 0;
+        public static int pausedTorrents = 0;
+        public static string[] torrentName = new string[100];
+        public static string[] torrentETA = new string[100];
+        public static string[] torrentDown = new string[100];
+        public static string[] torrentUp = new string[100];
+        public static string[] torrentSize = new string[100];
+        public static string[] torrentPeers = new string[100];
+        public static string[] torrentPeersConnected = new string[100];
+        public static string[] torrentSeeds = new string[100];
+        public static string[] torrentProgress = new string[100];
+    }
+}
